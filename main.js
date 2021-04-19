694a60c6c7d84f65378a715ee7fd80e40249299a
Your Name <you@example.com>
2021-04-19 14:24:56 +0200

    last

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 let carts = document.querySelectorAll('.add-cart');
 let product = [
     {
@@ -67,7 +69,7 @@ function setItem(product){
     localStorage.setItem("productInCart", JSON.stringify(cartItems));
 }
 function totalCost(product){
-    //console.log("the product price is", product.price);
+    
     let cartCost = localStorage.getItem('totalCost');
     
     console.log("my cartCost is", cartCost );
@@ -77,7 +79,47 @@ function totalCost(product){
         cartCost = parseFloat(cartCost);
         localStorage.setItem("totalCost",cartCost + product.price);
     } else {
-    localStorage.setItem("totalCost", product.price);
-} 
+        localStorage.setItem("totalCost", product.price);
+    } 
+
 }
+function displayCart() {
+    let cartItems = localStorage.getItem("productInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+    console.log(cartItems)
+    if( cartItems && productContainer ) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                <img src="1.jpeg"${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price"> ${item.price},00</div>
+            <div class="quantity"> 
+                <ion-icon name="caret-back-circle-outline"></ion-icon>
+                <span>${item.incart}</span>
+                <ion-icon name="caret-forward-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.incart * item.price},00  
+            </div>
+            `
+        });
+        productContainer.innerHTML +=`
+            <div class="basketTotalContainer">
+                <h4 class="bascketTotalTitle">
+                    basket Total
+                </h4>
+                <h4 class="basketTotal">
+                    $${cartCost},00
+                </h4>
+        `
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
